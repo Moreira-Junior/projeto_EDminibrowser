d8f8eb723517efda9d1dcc1aa7caa017d4dbb2ce
Moreira-Junior <junior_moreira88@hotmail.com>
2021-12-08 22:13:08 -0300

    correcao_ent_igual_vazio

--- main.py
+++ main.py
@@ -50,16 +50,9 @@ while True:
     except:
         pass
     
-    if ent=='':
-        while True:
-            print('Digite uma página: ')
-            ent=input()
-            ent=ent.lower() 
-            if ent!='':
-                break
 
     #funcionalidade back, retornando a página anterior na pilha  
-    elif ent=='#back':
+    if ent=='#back':
         if navegador1.pilha_vazia():
             print('Não há página no histórico ainda!')
         else:
@@ -70,7 +63,23 @@ while True:
             else:
                 navegador1.__init__('sites.txt')
                 del url
-    
+
+    #funcionalidade para informar quais comandos poderão ser utilizados pelo usuário
+    elif ent=='#help' or ent=='':
+        print('''================Help do navegador================
+        comando:                função:
+        #sair                   finaliza o navegador
+        #add                    adiciona uma página
+        #back                   voltar para página anterior
+        #showhist               mostrar o histórico de navegação
+        ''')
+        try:
+            if navegador1.topo_pilha()==url:
+                navegador1.voltar()
+        except:
+            pass
+        input('Pressione ENTER para continuar')
+
     #funcionalidade add, adicionando uma nova url e escrevendo-a no banco de urls em txt
     elif ent.split(' ')[0]=='#add':    
         try:
@@ -95,22 +104,6 @@ while True:
     #funcionalidade para imprimir o histórico de páginas visitadas
     elif ent=='#showhist':
         print(navegador1.mostra_historico())
-        try:
-            if navegador1.topo_pilha()==url:
-                navegador1.voltar()
-        except:
-            pass
-        input('Pressione ENTER para continuar')
-
-    #funcionalidade para informar quais comandos poderão ser utilizados pelo usuário
-    elif ent=='#help':
-        print('''================Help do navegador================
-        comando:                função:
-        #sair                   finaliza o navegador
-        #add                    adiciona uma página
-        #back                   voltar para página anterior
-        #showhist               mostrar o histórico de navegação
-        ''')
         try:
             if navegador1.topo_pilha()==url:
                 navegador1.voltar()
